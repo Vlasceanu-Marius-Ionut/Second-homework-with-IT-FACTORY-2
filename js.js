8d2a754a89ffbba8b4b7150cb8f20d5672f422f7
Vlasceanu Marius-Ionut <128095775+Vlasceanu-Marius-Ionut@users.noreply.github.com>
2023-07-13 21:55:16 +0300

    Second

---
+++ js.js
@@ -0,0 +1,10 @@
+function red(){
+  document.getElementById("redish").style.cssText = "background-color:red";
+  document.getElementById("blueish").style.cssText = "background-color:blue";
+  document.getElementById("yellow").style.cssText = "background-color:yellow";
+  document.getElementById("green").style.cssText = "background-color:green";
+  document.getElementById("purple").style.cssText = "background-color:purple";
+  document.getElementById("violet").style.cssText = "background-color:whitesmoke";
+  document.getElementById("gray").style.cssText = "background-color:grey";
+  document.getElementById("navy").style.cssText = "background-color:aqua";
+}
